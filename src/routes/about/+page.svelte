37b32b603b5116f44cf805691b78c3527e8d77f0
Stef Geraets <>
2023-10-11 23:42:15 +0200

    Add handy links in about and make ready for vercel deployment

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -16,10 +16,14 @@
         <li>Images: <a target="_blank" href="https://www.pexels.com/" rel="external">Pexels</a></li>
       </ul>
 
-      <h3>Repo's</h3>
+      <h3>API's and techiques</h3>
       <ul>
-        <li>Starting Repo: <a target="_blank" href="https://github.com" rel="external">Repo</a></li>
-        <li>Final Repo (with animations): <a target="_blank" href="https://github.com" rel="external">Repo</a></li>
+        <li>View transitions: <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/API/View_Transitions_API" rel="external">MDN</a></li>
+        <li>View transitions: <a target="_blank" href="https://developer.chrome.com/docs/web-platform/view-transitions/" rel="external">Google Developer</a></li>
+        <li>Scroll animations: <a target="_blank" href="https://developer.chrome.com/articles/scroll-driven-animations/" rel="external">Chome</a></li>
+        <li>Scroll Demo: <a target="_blank" href="https://scroll-driven-animations.style/" rel="external">Bramus</a></li>
+        <li>Scroll Tool: <a target="_blank" href="https://scroll-driven-animations.style/tools/view-timeline/ranges/#range-start-name=cover&range-start-percentage=0&range-end-name=cover&range-end-percentage=100&view-timeline-axis=block&view-timeline-inset=0&subject-size=smaller&subject-animation=reveal&interactivity=clicktodrag&show-areas=yes&show-fromto=yes&show-labels=yes" rel="external">Tool</a></li>
+        <li>Awesome Demo: <a target="_blank" href="https://live-transitions-sda.netlify.app/" rel="external">By Bramus</a></li>
       </ul>
     </div>
     <div>
@@ -29,6 +33,12 @@
         <li>Colors: <a target="_blank" href="https://www.realtimecolors.com/" rel="external">Realtime Colors</a></li>
         <li>Icons: <a target="_blank" href="https://tabler-icons.io/" rel="external">Tabler</a></li>
         <li>Hosting: <a target="_blank" href="https://vercel.com/" rel="external">Vercel</a></li>
+      </ul>
+
+      <h3>Repo's</h3>
+      <ul>
+        <li>Starting Repo: <a target="_blank" href="https://github.com" rel="external">Repo</a></li>
+        <li>Final Repo (with animations): <a target="_blank" href="https://github.com" rel="external">Repo</a></li>
       </ul>
     </div>
   </section>
